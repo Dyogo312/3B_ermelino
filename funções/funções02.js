8e31600cd62941121bf7feeb94514f4165ae6136
Dyogo312 <dyogodoss4ntos@gmail.com>
2024-10-25 19:38:34 +0000

    funções anônimas e nomeadas

---
+++ funções/funções02.js
@@ -0,0 +1,30 @@
+//funções nomeadas
+function logar(usuario, senha) {
+    alert('logado com a função nomeada!')
+    alert('Olá,' + usuario)
+
+}
+logar("Kabral", 4002)
+
+
+
+
+
+
+
+
+
+
+
+//funções anônimas
+const conectar = function(usuario, senha){
+    alert('Logado com a função anônima!')
+    alert('Olá,' + usuario)
+}
+
+
+
+
+
+
+//arrow functions ou funções flecha
