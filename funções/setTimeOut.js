6d3f039ca8ca56c0530424ec286b6914b05b6c6b
Dyogo312 <dyogodoss4ntos@gmail.com>
2024-11-01 18:20:02 +0000

    Clear interval e setinterval do Javascript

--- funções/setTimeOut.js
+++ funções/setTimeOut.js
@@ -2,4 +2,17 @@ setTimeout(function(){
     console.log("Tempo esgotado")
 }, 1000)
 
+//setInterval (função, tempo ms)
+ let Fps = Math.round(Math.random() * 30)
+ const intervalo1 =  setInterval (function() {
+    console.clear()
+     console.log("Fps: ", + Fps)
+     Fps = Fps +1
+ }, 1000)
+
+ setTimeout(function(){
+    clearInterval(intervalo1)
+    console.log("Conexão encerrada!")
+ }, 5000)
+
 
